a539b21bf3b2ab2206fe176b4f6d4b9f565b55a5
MCDLCN <martin.desbois@le-campus-numerique.fr>
2026-04-28 12:26:35 +0200

    Map working

---
+++ vite-project/src/components/CitiesMap.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+</script>
+
+<template>
+  <l-map style="height: 800px" :zoom="5" :center="[48.8566, 2.3522]">
+    <l-tile-layer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <l-marker :lat-lng="[48.8566, 2.3522]" />
+  </l-map>
+</template>
--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CitiesList from '../views/CitiesList.vue'
 import City from '../components/City.vue'
+import CitiesMap from '../components/CitiesMap.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/cities', component: CitiesList },
-    { path: '/city', component: City }
+    { path: '/city', component: City },
+    { path: '/map', component: CitiesMap}
   ]
 })
 
